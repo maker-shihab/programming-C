663bb3e341d3d64518cbc7c997a159d62c4b5142
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-10 14:41:36 +0600

    Basic intor, print 'Hello World'

---
+++ intro.c
@@ -0,0 +1,17 @@
+// Basyc Syntax
+
+#include <stdio.h>
+
+int main() {
+
+  printf("Hello Maker!");
+  
+  return 0;
+}
+
+// C Programming Comments
+
+// Here is Single Line Comment
+
+/* Here is multiple line 
+Comment */
