5823850b0cc79df6e01592193f456fc710085fd3
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-06-23 16:10:49 +0600

    Add Bubble Sort

--- algorithm.c
+++ algorithm.c
@@ -1,6 +1,42 @@
 // Here are some commonly used sorting algorithms:
 
 // Bubble Sort
+#include <stdio.h>
+
+void bubbleSort(int arr[], int size) {
+    int i, j, temp;
+    
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 8, 12, 1, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int i;
+    
+    printf("Original array: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    bubbleSort(arr, size);
+    
+    printf("\nSorted array: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
 
 // Selection Sort:
 
